59d40202c224e26f9f20eaf600a3dd4d4577928e
Gali Geller <gali1998@gmail.com>
2020-10-19 14:29:50 +0300

    checking

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import './App.css';
 import IdScreenComponent from './Components/IdScreenComponent';
 
-
 class App extends Component{
   constructor(props){
     super(props);
